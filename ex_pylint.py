54907aa8707c7b2e2b0f9c164cd12287e9d42e26
programadorLhama <programadorLhama@gmail.com>
2024-04-15 08:09:40 -0300

    config: Adding Pylint to project

---
+++ ex_pylint.py
@@ -0,0 +1,8 @@
+print("Ola Mundo")
+
+def minha_funcao():
+    print("Estou na minha funcao")
+
+# snake_case -> Variaveis e funções em Python: my_variable V
+# cammelCase -> Nao consta: minhaFuncao xxxxxx
+# PascalCase -> Classe em Python
